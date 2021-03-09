90690f5250ca7d30cc7475c1b17ebba48d597883
Trần Thắng <tranquyetthang281@gmail.com>
2021-03-09 10:42:31 +0700

    Add

---
+++ ClubSoftware/Model/DataProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubSoftware.Model
+{
+    class DataProvider
+    {
+        private static DataProvider _ins;
+        public static DataProvider Ins { get { if (_ins == null) _ins = new DataProvider(); return _ins; } set { _ins = value; } }
+
+        //public ClubSoftwareEntities DB { get; set; }
+
+
+
+    }
+}
